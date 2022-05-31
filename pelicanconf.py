735d382880a76653dcd1f003f66f72f1b4059fce
Denis Buryi <denis@buryi.de>
2022-05-31 16:38:30 +0700

    my init change

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-TAG_URL = 'tag/{slug}'
-TAG_SAVE_AS = 'tag/{slug}/index.html'
+# TAG_URL = 'tag/{slug}'
+# TAG_SAVE_AS = 'tag/{slug}/index.html'
 
-ARTICLE_URL = 'blog/{slug}'
-ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
+# ARTICLE_URL = 'blog/{slug}'
+# ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
 
-ARCHIVES_SAVE_AS = 'archives/index.html'
+# ARCHIVES_SAVE_AS = 'archives/index.html'
 
-PAGINATED_DIRECT_TEMPLATES = ('archives',)
-PAGINATION_PATTERNS = ((1, '{base_name}/', '{base_name}/index.html'), (2, '{base_name}/page{number}/', '{base_name}/page{number}/index.html'))
+# PAGINATED_DIRECT_TEMPLATES = ('archives',)
+# PAGINATION_PATTERNS = ((1, '{base_name}/', '{base_name}/index.html'), (2, '{base_name}/page{number}/', '{base_name}/page{number}/index.html'))
 
 BLOG_START_YEAR = "2008"
 
-COMMENT_SYSTEM_ID = "20774"
+# COMMENT_SYSTEM_ID = "20774"
 
 ARCHIVES_TITLE = "Archives"
 COMMENTS_TITLE = "Comments"
